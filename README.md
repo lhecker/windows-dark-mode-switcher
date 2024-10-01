d934d7f9323681bfdbbec72b48db52065ed926ec
Leonard Hecker <leonard@hecker.io>
2024-10-01 14:20:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# A simple Windows dark/light mode switcher
+
+Every time you run this application it'll toggle between light and dark mode. It's just a few lines of code and 4KiB.
+
+As I wrote it for my own purposes, it's currently not configurable and does not handle the `ColorPrevalence` setting ("Show accent color on Start and taskbar"). That setting had to be disabled when the light theme was activated, but that's not necessary with the latest versions of Windows anymore.
---
+++ main.c
@@ -0,0 +1,33 @@
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#define IFR_WIN32(x)                        \
+    do {                                    \
+        DWORD err = (x);                    \
+        if (err != 0) {                     \
+            return HRESULT_FROM_WIN32(err); \
+        }                                   \
+    } while (0)
+
+#define IFR_WIN32_BOOL(x)                              \
+    do {                                               \
+        BOOL ok = (x);                                 \
+        if (!ok) {                                     \
+            return HRESULT_FROM_WIN32(GetLastError()); \
+        }                                              \
+    } while (0)
+
+DWORD mainCRTStartup(void)
+{
+    DWORD light = 0;
+    DWORD size = sizeof(light);
+    IFR_WIN32(RegGetValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"SystemUsesLightTheme", RRF_RT_REG_DWORD | RRF_ZEROONFAILURE, NULL, &light, &size));
+
+    light = !light;
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"AppsUseLightTheme", REG_DWORD, &light, sizeof(light)));
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"SystemUsesLightTheme", REG_DWORD, &light, sizeof(light)));
+
+    IFR_WIN32_BOOL(SendNotifyMessageW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"ImmersiveColorSet"));
+    return 0;
+}
