60cd42a58c0cf6a0416a49801625f578b731069f
steffen <neffets720126@web.de>
2024-10-25 20:51:27 +0200

    make register work, add dynamic sunset / sunrise

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#define INITGUID
-#include <Windows.h>
+#include "dark_mode_switcher.h"
+
 #include <shellapi.h>
 #include <taskschd.h>
 
 #include <stdbool.h>
+#include <string.h>
+#include <wchar.h>
 
 // Curiously, the Windows SDK doesn't define these.
 DEFINE_GUID(IID_ITaskService, 0x2faba4c7, 0x4da9, 0x4013, 0x96, 0x97, 0x20, 0xcc, 0x3f, 0xd4, 0x0f, 0x85);
@@ -54,16 +54,6 @@ static bool string_equal(const wchar_t* a, const wchar_t* b)
     return result == CSTR_EQUAL;
 }
 
-// strlen() for people without stdlib.
-static size_t string_length(const wchar_t* str)
-{
-    size_t len = 0;
-    while (str[len] != L'\0') {
-        len++;
-    }
-    return len;
-}
-
 // Just a tiny helper to do argc/argv with bounds checking.
 static const wchar_t* get_arg(int argc, wchar_t** argv, int index, const wchar_t* fallback)
 {
@@ -73,37 +63,17 @@ static const wchar_t* get_arg(int argc, wchar_t** argv, int index, const wchar_t
     return argv[index];
 }
 
-// Validates that the input is HH:mm and returns 12:34 as 1234. Returns -1 on error.
-static int parse_time(const wchar_t* arg)
-{
-    if (string_length(arg) != 5) {
-        return -1;
-    }
-
-    if (arg[2] != L':') {
-        return -1;
-    }
-
-    int hour = (arg[0] - L'0') * 10 + (arg[1] - L'0');
-    int minute = (arg[3] - L'0') * 10 + (arg[4] - L'0');
-    if (hour < 0 || hour >= 24 || minute < 0 || minute >= 60) {
-        return -1;
-    }
-
-    return hour * 100 + minute;
-}
-
 static DWORD do_switch(int argc, wchar_t** argv)
 {
-    int sunrise = parse_time(get_arg(argc, argv, 2, L""));
-    int sunset = parse_time(get_arg(argc, argv, 3, L""));
-
-    if (sunrise < 0 || sunset < 0) {
+    SYSTEMTIME sunrise, sunset;
+    if (!string_to_time(&sunrise, get_arg(argc, argv, 2, L"")) || !string_to_time(&sunset, get_arg(argc, argv, 3, L""))) {
         print_stdout_lit("Invalid time format. Use HH:mm.\r\n");
         return 1;
     }
 
-    if (sunrise >= sunset) {
+    const int sunrise_minutes = sunrise.wHour * 60 + sunrise.wMinute;
+    const int sunset_minutes = sunset.wHour * 60 + sunset.wMinute;
+    if (sunrise_minutes >= sunset_minutes) {
         print_stdout_lit("Sunrise must be before sunset.\r\n");
         return 1;
     }
@@ -111,8 +81,7 @@ static DWORD do_switch(int argc, wchar_t** argv)
     SYSTEMTIME time;
     GetLocalTime(&time);
 
-    int current_time = time.wHour * 100 + time.wMinute;
-    DWORD light = current_time >= sunrise && current_time < sunset;
+    const DWORD light = get_use_light(&time, &sunrise, &sunset);
     IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"AppsUseLightTheme", REG_DWORD, &light, sizeof(light)));
     IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"SystemUsesLightTheme", REG_DWORD, &light, sizeof(light)));
 
@@ -131,26 +100,37 @@ static BSTR get_exe_path()
     return SysAllocString(exe_path_buf);
 }
 
-static DWORD do_register(int argc, wchar_t** argv)
+static DWORD do_register_time(int argc, wchar_t** argv)
 {
-    int sunrise = parse_time(get_arg(argc, argv, 2, L""));
-    int sunset = parse_time(get_arg(argc, argv, 3, L""));
+    SYSTEMTIME time;
+    GetLocalTime(&time);
 
-    if (sunrise < 0 || sunset < 0) {
+    SYSTEMTIME sunrise = time, sunset = time;
+    sunrise.wSecond = sunset.wSecond = 0;
+    sunrise.wMilliseconds = sunset.wMilliseconds = 0;
+    if (!string_to_time(&sunrise, get_arg(argc, argv, 2, L"")) || !string_to_time(&sunset, get_arg(argc, argv, 3, L""))) {
         print_stdout_lit("Invalid time format. Use HH:mm.\r\n");
         return 1;
     }
 
-    if (sunrise >= sunset) {
+    const int sunrise_minutes = sunrise.wHour * 60 + sunrise.wMinute;
+    const int sunset_minutes = sunset.wHour * 60 + sunset.wMinute;
+    if (sunrise_minutes >= sunset_minutes) {
         print_stdout_lit("Sunrise must be before sunset.\r\n");
         return 1;
     }
+
+    const DWORD light = get_use_light(&time, &sunrise, &sunset);
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"AppsUseLightTheme", REG_DWORD, &light, sizeof(light)));
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"SystemUsesLightTheme", REG_DWORD, &light, sizeof(light)));
+
+    IFR_WIN32_BOOL(SendNotifyMessageW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"ImmersiveColorSet"));
 
     BSTR exe_path = get_exe_path();
     if (!exe_path) {
         return HRESULT_FROM_WIN32(GetLastError());
     }
-    
+
     wchar_t user_id[256];
     DWORD user_id_size = ARRAYSIZE(user_id);
     IFR_WIN32_BOOL(GetUserNameW(user_id, &user_id_size));
@@ -159,7 +139,7 @@ static DWORD do_register(int argc, wchar_t** argv)
 
     ITaskService* pService = NULL;
     IFR(CoCreateInstance(&CLSID_TaskScheduler, NULL, CLSCTX_INPROC_SERVER, &IID_ITaskService, (void**)&pService));
-    IFR(pService->lpVtbl->Connect(pService, (VARIANT){}, (VARIANT){}, (VARIANT){}, (VARIANT){}));
+    IFR(pService->lpVtbl->Connect(pService, (VARIANT){0}, (VARIANT){0}, (VARIANT){0}, (VARIANT){0}));
 
     ITaskDefinition* pTask = NULL;
     IFR(pService->lpVtbl->NewTask(pService, 0, &pTask));
@@ -178,23 +158,35 @@ static DWORD do_register(int argc, wchar_t** argv)
     ITriggerCollection* pTriggerCollection = NULL;
     IFR(pTask->lpVtbl->get_Triggers(pTask, &pTriggerCollection));
 
-    //  Add the daily trigger to the task.
+    //  Add the daily sunrise trigger to the task.
     ITrigger* pTriggerSunrise = NULL;
     IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_DAILY, &pTriggerSunrise));
     IDailyTrigger* pDailyTriggerSunrise = NULL;
     IFR(pTriggerSunrise->lpVtbl->QueryInterface(pTriggerSunrise, &IID_IDailyTrigger, (void**)&pDailyTriggerSunrise));
     IFR(pDailyTriggerSunrise->lpVtbl->put_ExecutionTimeLimit(pDailyTriggerSunrise, SysAllocString(L"PT1M")));
-    IFR(pDailyTriggerSunrise->lpVtbl->put_StartBoundary(pDailyTriggerSunrise, SysAllocString(L"2005-01-01T12:05:00")));
+    wchar_t rise_buffer[20];
+    time_to_string(rise_buffer, ARRAYSIZE(rise_buffer), &sunrise);
+    IFR(pDailyTriggerSunrise->lpVtbl->put_StartBoundary(pDailyTriggerSunrise, SysAllocString(rise_buffer)));
     IFR(pDailyTriggerSunrise->lpVtbl->put_DaysInterval(pDailyTriggerSunrise, 1));
 
-    //  Add the daily trigger to the task.
+    //  Add the daily sunset trigger to the task.
     ITrigger* pTriggerSunset = NULL;
     IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_DAILY, &pTriggerSunset));
     IDailyTrigger* pDailyTriggerSunset = NULL;
     IFR(pTriggerSunset->lpVtbl->QueryInterface(pTriggerSunset, &IID_IDailyTrigger, (void**)&pDailyTriggerSunset));
     IFR(pDailyTriggerSunset->lpVtbl->put_ExecutionTimeLimit(pDailyTriggerSunset, SysAllocString(L"PT1M")));
-    IFR(pDailyTriggerSunset->lpVtbl->put_StartBoundary(pDailyTriggerSunset, SysAllocString(L"2005-01-01T12:05:00")));
+    wchar_t set_buffer[20];
+    time_to_string(set_buffer, ARRAYSIZE(set_buffer), &sunset);
+    IFR(pDailyTriggerSunset->lpVtbl->put_StartBoundary(pDailyTriggerSunset, SysAllocString(set_buffer)));
     IFR(pDailyTriggerSunset->lpVtbl->put_DaysInterval(pDailyTriggerSunset, 1));
+
+    //  Add the logon trigger to the task.
+    ITrigger* pTriggerLogon = NULL;
+    IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_LOGON, &pTriggerLogon));
+    ILogonTrigger* pLogonTrigger = NULL;
+    IFR(pTriggerLogon->lpVtbl->QueryInterface(pTriggerLogon, &IID_ILogonTrigger, (void**)&pLogonTrigger));
+    IFR(pLogonTrigger->lpVtbl->put_ExecutionTimeLimit(pLogonTrigger, SysAllocString(L"PT1M")));
+    IFR(pLogonTrigger->lpVtbl->put_UserId(pLogonTrigger, SysAllocString(user_id)));
 
     // Add an action to the task.
     IActionCollection* pActionCollection = NULL;
@@ -204,12 +196,138 @@ static DWORD do_register(int argc, wchar_t** argv)
     IExecAction* pExecAction = NULL;
     IFR(pAction->lpVtbl->QueryInterface(pAction, &IID_IExecAction, (void**)&pExecAction));
     IFR(pExecAction->lpVtbl->put_Path(pExecAction, exe_path));
+    wchar_t args[20] = L"switch            "; // keep the number of spaces! the task only switchs the mode, so the command to run is `switch`
+    memcpy(&args[7], argv[2], 10);
+    memcpy(&args[13], argv[3], 10);
+    IFR(pExecAction->lpVtbl->put_Arguments(pExecAction, SysAllocString(args)));
 
     // Save the task in the root folder.
     ITaskFolder* pRootFolder = NULL;
     IFR(pService->lpVtbl->GetFolder(pService, SysAllocString(L"\\"), &pRootFolder));
     IRegisteredTask* pRegisteredTask = NULL;
-    IFR(pRootFolder->lpVtbl->RegisterTaskDefinition(pRootFolder, SysAllocString(L"Dark Mode Switcher"), pTask, TASK_CREATE_OR_UPDATE, (VARIANT){}, (VARIANT){}, TASK_LOGON_INTERACTIVE_TOKEN, (VARIANT){}, &pRegisteredTask));
+    IFR(pRootFolder->lpVtbl->RegisterTaskDefinition(pRootFolder, SysAllocString(L"Dark Mode Switcher"), pTask, TASK_CREATE_OR_UPDATE, (VARIANT){0}, (VARIANT){0}, TASK_LOGON_INTERACTIVE_TOKEN, (VARIANT){0}, &pRegisteredTask));
+
+    return 0;
+}
+
+static DWORD do_register_position(int argc, wchar_t** argv)
+{
+    double lat = 0.0, lon = 0.0;
+    if (!string_to_coordinate(&lat, get_arg(argc, argv, 2, L"_"), 90.0, -90.0) || !string_to_coordinate(&lon, get_arg(argc, argv, 3, L"_"), 180.0, -180.0)) {
+        return 1;
+    }
+
+    SYSTEMTIME rise = { 0 }, set = { 0 }, nextrise = { 0 }, nextset = { 0 };
+
+    const int rc = get_sun_rise_set(&rise, &set, &nextrise, &nextset, lat, lon);
+    if (rc == sun_error) {
+        return 1;
+    }
+
+    // the current mode is based on today's sunrise and sunset
+    DWORD light;
+    if (rc & sun_normal) {
+        SYSTEMTIME now;
+        GetLocalTime(&now);
+        light = get_use_light(&now, &rise, &set);
+    } else if (rc & sun_above_horizon) {
+        light = 1;
+    } else {
+        light = 0;
+    }
+
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"AppsUseLightTheme", REG_DWORD, &light, sizeof(light)));
+    IFR_WIN32(RegSetKeyValueW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", L"SystemUsesLightTheme", REG_DWORD, &light, sizeof(light)));
+
+    IFR_WIN32_BOOL(SendNotifyMessageW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L"ImmersiveColorSet"));
+
+    BSTR exe_path = get_exe_path();
+    if (!exe_path) {
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    wchar_t user_id[256];
+    DWORD user_id_size = ARRAYSIZE(user_id);
+    IFR_WIN32_BOOL(GetUserNameW(user_id, &user_id_size));
+
+    // the scheduled task is based on the next sunrise and sunset that can still be one or even both of today's values
+    IFR(CoInitializeEx(NULL, COINIT_MULTITHREADED));
+
+    ITaskService* pService = NULL;
+    IFR(CoCreateInstance(&CLSID_TaskScheduler, NULL, CLSCTX_INPROC_SERVER, &IID_ITaskService, (void**)&pService));
+    IFR(pService->lpVtbl->Connect(pService, (VARIANT){0}, (VARIANT){0}, (VARIANT){0}, (VARIANT){0}));
+
+    ITaskDefinition* pTask = NULL;
+    IFR(pService->lpVtbl->NewTask(pService, 0, &pTask));
+
+    ITaskSettings* pSettings = NULL;
+    IFR(pTask->lpVtbl->get_Settings(pTask, &pSettings));
+    IFR(pSettings->lpVtbl->put_AllowDemandStart(pSettings, VARIANT_FALSE));
+    IFR(pSettings->lpVtbl->put_StartWhenAvailable(pSettings, VARIANT_TRUE));
+
+    // Get the current user ID.
+    IPrincipal* pPrincipal = NULL;
+    IFR(pTask->lpVtbl->get_Principal(pTask, &pPrincipal));
+    IFR(pPrincipal->lpVtbl->put_UserId(pPrincipal, SysAllocString(user_id)));
+    IFR(pPrincipal->lpVtbl->put_LogonType(pPrincipal, TASK_LOGON_INTERACTIVE_TOKEN));
+
+    ITriggerCollection* pTriggerCollection = NULL;
+    IFR(pTask->lpVtbl->get_Triggers(pTask, &pTriggerCollection));
+
+    //  Add the sunrise trigger to the task.
+    ITrigger* pTriggerSunrise = NULL;
+    IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_DAILY, &pTriggerSunrise));
+    IDailyTrigger* pDailyTriggerSunrise = NULL;
+    IFR(pTriggerSunrise->lpVtbl->QueryInterface(pTriggerSunrise, &IID_IDailyTrigger, (void**)&pDailyTriggerSunrise));
+    IFR(pDailyTriggerSunrise->lpVtbl->put_ExecutionTimeLimit(pDailyTriggerSunrise, SysAllocString(L"PT1M")));
+    wchar_t rise_buffer[20];
+    time_to_string(rise_buffer, ARRAYSIZE(rise_buffer), &nextrise);
+    IFR(pDailyTriggerSunrise->lpVtbl->put_StartBoundary(pDailyTriggerSunrise, SysAllocString(rise_buffer)));
+    IFR(pDailyTriggerSunrise->lpVtbl->put_DaysInterval(pDailyTriggerSunrise, 1));
+
+    //  Add the sunset trigger to the task.
+    ITrigger* pTriggerSunset = NULL;
+    IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_DAILY, &pTriggerSunset));
+    IDailyTrigger* pDailyTriggerSunset = NULL;
+    IFR(pTriggerSunset->lpVtbl->QueryInterface(pTriggerSunset, &IID_IDailyTrigger, (void**)&pDailyTriggerSunset));
+    IFR(pDailyTriggerSunset->lpVtbl->put_ExecutionTimeLimit(pDailyTriggerSunset, SysAllocString(L"PT1M")));
+    wchar_t set_buffer[20];
+    time_to_string(set_buffer, ARRAYSIZE(set_buffer), &nextset);
+    IFR(pDailyTriggerSunset->lpVtbl->put_StartBoundary(pDailyTriggerSunset, SysAllocString(set_buffer)));
+    IFR(pDailyTriggerSunset->lpVtbl->put_DaysInterval(pDailyTriggerSunset, 1));
+
+    //  Add the logon trigger to the task.
+    ITrigger* pTriggerLogon = NULL;
+    IFR(pTriggerCollection->lpVtbl->Create(pTriggerCollection, TASK_TRIGGER_LOGON, &pTriggerLogon));
+    ILogonTrigger* pLogonTrigger = NULL;
+    IFR(pTriggerLogon->lpVtbl->QueryInterface(pTriggerLogon, &IID_ILogonTrigger, (void**)&pLogonTrigger));
+    IFR(pLogonTrigger->lpVtbl->put_ExecutionTimeLimit(pLogonTrigger, SysAllocString(L"PT1M")));
+    IFR(pLogonTrigger->lpVtbl->put_UserId(pLogonTrigger, SysAllocString(user_id)));
+
+    // Add an action to the task.
+    IActionCollection* pActionCollection = NULL;
+    IFR(pTask->lpVtbl->get_Actions(pTask, &pActionCollection));
+    IAction* pAction = NULL;
+    IFR(pActionCollection->lpVtbl->Create(pActionCollection, TASK_ACTION_EXEC, &pAction));
+    IExecAction* pExecAction = NULL;
+    IFR(pAction->lpVtbl->QueryInterface(pAction, &IID_IExecAction, (void**)&pExecAction));
+    IFR(pExecAction->lpVtbl->put_Path(pExecAction, exe_path));
+    const size_t arglen2 = wcslen(argv[2]);
+    const size_t arglen3 = wcslen(argv[3]);
+    if (arglen2 + arglen3 > 490) {
+        return 1;
+    }
+    wchar_t args[512] = L"registerposition "; // the task has to update itself, so the command to run is `registerposition`
+    memcpy(&args[17], argv[2], arglen2 * sizeof(wchar_t));
+    args[17 + arglen2] = L' ';
+    memcpy(&args[18 + arglen2], argv[3], (arglen3 + 1) * sizeof(wchar_t));
+    IFR(pExecAction->lpVtbl->put_Arguments(pExecAction, SysAllocString(args)));
+
+    // Save the task in the root folder.
+    ITaskFolder* pRootFolder = NULL;
+    IFR(pService->lpVtbl->GetFolder(pService, SysAllocString(L"\\"), &pRootFolder));
+    IRegisteredTask* pRegisteredTask = NULL;
+    IFR(pRootFolder->lpVtbl->RegisterTaskDefinition(pRootFolder, SysAllocString(L"Dark Mode Switcher"), pTask, TASK_CREATE_OR_UPDATE, (VARIANT){0}, (VARIANT){0}, TASK_LOGON_INTERACTIVE_TOKEN, (VARIANT){0}, &pRegisteredTask));
 
     return 0;
 }
@@ -223,24 +341,28 @@ static DWORD do_help(DWORD status)
         "  help, -h, --help\r\n"
         "    Print help\r\n"
         "  switch <SUNRISE> <SUNSET>\r\n"
-        "    Switch between light/dark mode. SUNRISE and SUNSET must be in the HH:mm 24-hour format. Prefer using the register command.\r\n"
-        "  register <SUNRISE> <SUNSET>\r\n"
+        "    Switch between light/dark mode. SUNRISE and SUNSET must be in the HH:mm 24-hour format. Prefer using the registertime command.\r\n"
+        "  registertime <SUNRISE> <SUNSET>\r\n"
         "    Sets a scheduled task for the light/dark switch mode. SUNRISE and SUNSET must be in the HH:mm 24-hour format.\r\n"
+        "  registerposition <LATITUDE> <LONGITUDE>\r\n"
+        "    Dynamically sets a scheduled task switching the light/dark mode based on the geographic coordinates.\r\n"
+        "    LATITUDE and LONGITUDE must be in the Digital Degrees format.\r\n"
     );
     return status;
 }
 
-DWORD mainCRTStartup()
+int wmain(int argc, wchar_t* argv[])
 {
-    int argc;
-    wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
     const wchar_t* argv1 = get_arg(argc, argv, 1, L"help");
 
     if (string_equal(argv1, L"switch")) {
         return do_switch(argc, argv);
     }
-    if (string_equal(argv1, L"register")) {
-        return do_register(argc, argv);
+    if (string_equal(argv1, L"registertime")) {
+        return do_register_time(argc, argv);
+    }
+    if (string_equal(argv1, L"registerposition")) {
+        return do_register_position(argc, argv);
     }
     if (string_equal(argv1, L"help") || string_equal(argv1, L"-h") || string_equal(argv1, L"--help")) {
         return do_help(0);
