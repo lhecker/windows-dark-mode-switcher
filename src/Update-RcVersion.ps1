ee12ea41536fee4d34b89b4323cd8f67f5d2d8f2
Steffen <neffets720126@web.de>
2025-03-06 16:25:15 +0100

    Make versioning easier (#14)
    - GitHub automatically creates a URL that redirects to the recent
      release. So no further update of the link in the README file needed.
    - The new helper script can be run from within an explorer window.
      In this case it prompts the user to enter `major`, `minor` and `patch`
      numbers. Both the file and the product versions are updated in
      `resource.rc`. The updated contents are also printed to the terminal
      with the changed lines highlighted.

---
+++ src/Update-RcVersion.ps1
@@ -0,0 +1,65 @@
+<#
+  .SYNOPSIS
+  Updates the version numbers in *.rc files.
+
+  .DESCRIPTION
+  Updates major version, minor version, patch number and revision number of
+  both the file version and the product version specified in resource files.
+
+  .PARAMETER major
+  Specifies the new major version.
+
+  .PARAMETER minor
+  Specifies the new minor version.
+
+  .PARAMETER patch
+  Specifies the new patch number.
+
+  .PARAMETER revision
+  [optional] Specifies the new revision number.
+
+  .PARAMETER rcfiles
+  [optional] Specifies a list of paths of the resource files to update.
+
+  .INPUTS
+  [optional] The list of paths of the resource files to update.
+
+  .OUTPUTS
+  The new content of the resource file.
+
+  .NOTES
+  The script can be executed without any arguments, where the user is prompted
+  to enter the values for mandatory parameters. If needed, change the default
+  value of the 'rcfiles' parameter accordingly.
+  The old content of the resource file is overwritten. In addition, the updated
+  content can be instantly checked in the terminal window. Changed lines are
+  highlighted.
+#>
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory=$true)][UInt16]$major,
+  [Parameter(Mandatory=$true)][UInt16]$minor,
+  [Parameter(Mandatory=$true)][UInt16]$patch,
+  [UInt16]$revision = 0,
+  [Parameter(ValueFromPipeline=$true)][string[]]$rcfiles = 'resource.rc'
+)
+begin {
+  $regex = '(^[^/]*\b(?:file|product)version[", ]+)\d+([,.])\d+([,.])\d+([,.])\d+\b(.*$)'
+  $subst = "`${1}$major`${2}$minor`${3}$patch`${4}$revision`${5}"
+  $e = [Char]27
+}
+process {
+  ForEach ($rcfile in $rcfiles) {
+    if ((-not (Test-Path $rcfile -PathType Leaf)) -or ($rcfile -inotlike '*.rc')) {continue}
+    (Get-Content $rcfile) -ireplace $regex, $subst | Tee-Object $rcfile |
+     ForEach-Object {"$e[90m | $e[0m$($_ -ireplace $regex, "$e[97;41m`${0}$e[0m")"} -Begin {"`n$e[90m Updated `"$rcfile`":$e[0m"} -End {''}
+  }
+}
+end {
+  try {
+    $instances = Get-CimInstance Win32_Process -ErrorAction Stop -Property @('Name', 'ParentProcessId', 'ProcessId')
+    $parentpid = $instances | Where-Object ProcessId -eq $PID | Select-Object -ExpandProperty ParentProcessId
+    $parentname = $instances | Where-Object ProcessId -eq $parentpid | Select-Object -ExpandProperty Name
+    if ($parentname -imatch '.+cmd|commander|dopus|far|files|plorer|q-dir') {pause}
+  } catch {exit 0}
+}
